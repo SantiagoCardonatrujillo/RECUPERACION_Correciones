71dd7a85e653b93d37ef2b6bc9f8b137fd2e5f81
SantiagoCardonatrujillo <118689812+SantiagoCardonatrujillo@users.noreply.github.com>
2022-11-22 18:55:19 -0500

    Add files via upload

---
+++ recuperacion MT/java/contador.js
@@ -0,0 +1,99 @@
+var boton1 = document.getElementById('btn1');
+var boton2 = document.getElementById('btn2');
+var boton3 = document.getElementById('btn3');
+var boton4 = document.getElementById('btn4');
+var boton5 = document.getElementById('btn5');
+var boton6 = document.getElementById('btn6');
+var boton7 = document.getElementById('btn7');
+var boton8 = document.getElementById('btn8');
+var boton9 = document.getElementById('btn9');
+var boton10 = document.getElementById('btn10');
+var boton11 = document.getElementById('btn11');
+var boton12 = document.getElementById('btn12');
+var boton13 = document.getElementById('btn13');
+var boton14 = document.getElementById('btn14');
+var contador = document.getElementById('contar');
+var numero=0;
+
+boton1.onclick = function(){
+   numero++;
+   contador.textContent = numero;
+}
+
+
+boton2.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton3.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton4.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton5.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton6.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton7.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton8.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton9.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton10.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+ 
+ boton11.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton12.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+
+ boton13.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
+
+ 
+ boton14.onclick = function(){
+    numero++;
+    contador.textContent = numero;
+ }
